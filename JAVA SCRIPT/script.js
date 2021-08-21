b97e242e1cc1babfc4f6da7d6946e0754d54deeb
ayush-code-drops <ayushmangal121212@gmail.com>
2021-08-21 13:06:33 +0530

    pricing page by Ayush

--- JAVA SCRIPT/script.js
+++ JAVA SCRIPT/script.js
@@ -6,20 +6,20 @@ var cont=document.getElementsByTagName('div')[0]
    
 //     navbar.style.top="0"
 // })
-var count = 0;
-if (window.scrollY > 50) {
-    count++
-}
-console.log(count)
-console.log(window.scrollY)
-// window.addEventListener('scroll', function () {
+// var count = 0;
+// if (window.scrollY > 50) {
+//     count++
+// }
+// console.log(count)
+// console.log(window.scrollY)
+window.addEventListener('scroll', function () {
 
-//     console.log(window.scrollY)
+    console.log(window.scrollY)
    
-//         if (window.scrollY>50) {
-//         navbar.style.top = "0"
+        if (window.scrollY>50) {
+        navbar.style.top = "0"
       
-//     }
+    }
         
         
-//     })
+    })
